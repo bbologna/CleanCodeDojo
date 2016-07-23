325a26c2bec1a1c69f33ec8c6eba1628d0c7f2b2
Bruno Bologna <bruno.bologna@hotmail.com>
2016-07-23 12:14:01 -0300

    Added Cohesion example

---
+++ 1/CleanCode/CleanCode/IWebApiService.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanCode
+{
+    internal interface IWebApiService
+    {
+        void Error(string error);
+        void Info(string error);
+        void Warning(string error);
+        void Trace(string error);
+    }
+}
---
+++ 1/CleanCode/CleanCode/NotificationService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    public class NotificationService
+    {
+        private IWebApiService service;
+
+        private IDbNotification DbNotififacion;
+
+        public void NotifyErrorWebApi(string error)
+        {
+            /*Do some stuff*/
+            service.Error(error);
+        }
+
+        public void NotifyErrorDb(string error)
+        {
+            /*Do some stuff*/
+            DbNotififacion.Error(error);
+        }
+
+        public void NotifyInfoWebApi(string error)
+        {
+            /*Do some stuff*/
+            service.Info(error);
+        }
+
+        public void NotifyInfoDb(string error)
+        {
+            /*Do some stuff*/
+            DbNotififacion.Info(error);
+        }
+
+        public void NotifyWarningWebApi(string error)
+        {
+            service.Warning(error);
+        }
+
+        public void NotifyWarningDb(string error)
+        {
+            /*Do some stuff*/
+            DbNotififacion.Warning(error);
+        }
+
+        public void NotifyTraceWebApi(string error)
+        {
+            /*Do some stuff*/
+            service.Trace(error);
+        }
+
+        public void NotifyTraceDb(string error)
+        {
+            /*Do some stuff*/
+            DbNotififacion.Trace(error);
+        }
+    }
+}
