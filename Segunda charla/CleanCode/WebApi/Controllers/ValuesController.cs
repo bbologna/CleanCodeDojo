e3e75acddc30d22438347d49cc2b3bf6afe9ca8f
Bruno Bologna <Bruno Bologna>
2016-12-19 17:24:56 -0300

    Added new projeect

---
+++ Segunda charla/CleanCode/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ItemController : ApiController
+    {
+        private static Dictionary<int,Item> inMemoryItems = new Dictionary<int,Item>();
+
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            return Ok(inMemoryItems.Values.ToList());
+        }
+
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(inMemoryItems[id]);
+        }
+
+        // POST api/values
+        public void Post(Item value)
+        {
+            inMemoryItems.Add(value.Id, value);
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
